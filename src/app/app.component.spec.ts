c315ab964b1e5479eb563297692abf02b8959aee
Alex Shtern <alexshtern1989@gmail.com>
2024-06-01 19:04:17 +0300

    add tests for holidays

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, BrowserAnimationsModule],
+      providers: [provideAnimations()],
     }).compileComponents();
   });
 
@@ -20,10 +25,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('israeli-holidays');
   });
 
-  it('should render title', () => {
+  it('should render holyday component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, israeli-holidays');
+    expect(compiled.querySelector('app-holidays')).not.toBeNull();
   });
 });
